3bcb5ec097c481ab5b1ecc9b0aed813a04ae3ea7
Anton-Desktop <kuvshinoffav@gmail.com>
2023-03-06 23:50:03 +0300

    kk

--- src/components/todo-list/index.jsx
+++ src/components/todo-list/index.jsx
@@ -1,12 +1,12 @@
-import {useEffect} from 'react';
-import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import {
   todosSelector,
   todosLoadingSelector,
   todosErrorSelector,
-} from "../../store/selectors/todo";
-import { Todo } from "../todo";
-import {fetchTodos} from '../../store/actions/thunks/todo';
+} from '../../store/selectors/todo';
+import { Todo } from '../todo';
+import { fetchTodos } from '../../store/actions/thunks/todo';
 
 import styles from './index.module.css';
 
@@ -16,24 +16,24 @@ export const TodoList = () => {
   const todos = useSelector(todosSelector);
   const loading = useSelector(todosLoadingSelector);
   const error = useSelector(todosErrorSelector);
-
+ 
   const isEmptyList = !loading && !todos?.length;
 
   useEffect(() => {
     dispatch(fetchTodos());
   }, []);
 
-   if (loading) {
-     return <p>Loading...</p>;
-   }
+  if (loading) {
+    return <p>Loading...</p>;
+  }
 
-   if (error) {
-     return <p>{error.message}</p>;
-   }
+  if (error) {
+    return <p>{error.message}</p>;
+  }
 
-   if (isEmptyList) {
-     return <p>No todos, yay!</p>;
-   }
+  if (isEmptyList) {
+    return <p>No todos, yay!</p>;
+  }
 
   return (
     <ul className={styles.list}>
@@ -43,3 +43,4 @@ export const TodoList = () => {
     </ul>
   );
 };
+
